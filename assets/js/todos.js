0a3d84124863f9735d168a967d8be1ad020168f4
Brian Jones <brijones.nz@gmail.com>
2019-12-10 13:45:34 +1300

    Up to date app

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,26 @@
 //Check off todo's 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
 //delete a todo
-$("span").click(function(event){
-    $(this).parent().remove();
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
     event.stopPropogation();
 });
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        //saves value in input
+        var todo = $(this).val();
+        //clears text from input
+        $(this).val("");
+        //creates todo in list
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> " + todo + "</li>")
+    }
+})
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+})
